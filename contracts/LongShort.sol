efded157e0dd264d0b559aee3da6166a5944bcdd
jonjon <jonjonclark@gmail.com>
2020-08-09 17:09:00 +0100

    Get basic interest generation in place (aave), create new coins

--- contracts/LongShort.sol
+++ contracts/LongShort.sol
@@ -2,8 +2,15 @@
 pragma solidity ^0.6.8;
 
 import "@nomiclabs/buidler/console.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorInterface.sol";
+
+import "./interfaces/IAaveLendingPool.sol";
+import "./interfaces/IADai.sol";
+import "./interfaces/ILendingPoolAddressesProvider.sol";
+
+import "./LongCoins.sol";
+import "./ShortCoins.sol";
 
 contract LongShort {
     using SafeMath for uint256;
@@ -11,12 +18,21 @@ contract LongShort {
     AggregatorInterface internal priceFeed;
 
     uint256 public totalValueLocked;
-    int256 public longValue;
-    int256 public shortValue;
+    int256 public longValue; // 11 000  $1.1 = 1 gov
+    int256 public shortValue; // 19 000 $0.9 = 1 gov
 
     uint256 public longTokenPrice;
-    uint256 public shortTokenPrice;
+    uint256 public shortTokenPrice; // gas costs.
     int256 public assetPrice;
+
+    LongCoins public longTokens;
+    ShortCoins public shortTokens;
+
+    IERC20 public daiContract;
+    IAaveLendingPool public aaveLendingContract;
+    IADai public adaiContract;
+    ILendingPoolAddressesProvider public provider;
+    address public aaveLendingContractCore;
 
     /**
      * Necessary to update system state before any contract actions (deposits / withdraws)
@@ -28,49 +44,34 @@ contract LongShort {
 
     /**
      * Network: Kovan
-     * Aggregator: ETH/USD
-     * Address: 0xD21912D8762078598283B14cbA40Cb4bFCb87581
+     * Aggregator: BTC/USD
+     * Address: 0x2445F2466898565374167859Ae5e3a231e48BB41
+     * TODO: weigh up pros/cons of making this upgradable
      */
-    constructor() public {
+    constructor(
+        address _longCoins,
+        address _shortCoins,
+        address daiAddress,
+        address aDaiAddress,
+        // lendingPoolAddressProvider should be one of below depending on deployment
+        // kovan 0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5
+        // mainnet 0x24a42fD28C976A61Df5D00D0599C34c4f90748c8
+        address lendingPoolAddressProvider
+    ) public {
         priceFeed = AggregatorInterface(
-            0xD21912D8762078598283B14cbA40Cb4bFCb87581
+            0x2445F2466898565374167859Ae5e3a231e48BB41
         );
-    }
+        // Will need to make sure we are a minter! and pauser!
+        longTokens = LongCoins(_longCoins);
+        shortTokens = ShortCoins(_shortCoins);
 
-    /**
-     * Updates the value of the long and short sides within the system
-     */
-    function _updateSystemState() internal {
-        int256 newPrice = getLatestPrice();
-        int256 percentageChange;
+        daiContract = IERC20(daiAddress);
+        provider = ILendingPoolAddressesProvider(lendingPoolAddressProvider);
+        adaiContract = IADai(aDaiAddress);
 
-        if (assetPrice == newPrice) {
-            return; // If no new price update from oracle, proceed as normal
-        }
-
-        int256 valueChange = 0;
-        // Long gains
-        if (newPrice > assetPrice) {
-            percentageChange = (newPrice - assetPrice) / assetPrice;
-            if (getShortPercentageFilled() == 1) {
-                valueChange = shortValue * percentageChange;
-            } else {
-                valueChange = longValue * percentageChange;
-            }
-            longValue = longValue + valueChange;
-            shortValue = shortValue - valueChange; // NB Check for going below zero and system instability
-        } else {
-            percentageChange = (assetPrice - newPrice) / assetPrice;
-            if (getShortPercentageFilled() == 1) {
-                valueChange = shortValue * percentageChange;
-            } else {
-                valueChange = longValue * percentageChange;
-            }
-            longValue = longValue - valueChange;
-            shortValue = shortValue + valueChange;
-        }
-
-        assetPrice = newPrice;
+        // Intialize price at $1 per token (adjust decimals)
+        longTokenPrice = 1;
+        shortTokenPrice = 1;
     }
 
     /**
@@ -90,7 +91,9 @@ contract LongShort {
     /**
      * Returns % of long position that is filled
      */
-    function getLongPercentageFilled() public view returns (int256) {
+    function getLongBeta() public view returns (int256) {
+        // TODO account for contract start when these are both zero
+        // and an erronous beta of 1 reported.
         if (shortValue >= longValue) {
             return 1;
         } else {
@@ -102,11 +105,78 @@ contract LongShort {
      * Returns % of short position that is filled
      * zero div  error if both are zero
      */
-    function getShortPercentageFilled() public view returns (int256) {
+    function getShortBeta() public view returns (int256) {
         if (longValue >= shortValue) {
             return 1;
         } else {
             return longValue / (shortValue);
         }
+    }
+
+    /**
+     * Updates the value of the long and short sides within the system
+     */
+    function _updateSystemState() internal {
+        // For system start, no value adjustment till positions on both sides exist
+        // Consider attacks of possible zero balances later on in contract life?
+        if (longValue == 0 || shortValue == 0) {
+            return;
+        }
+
+        int256 newPrice = getLatestPrice();
+        // If no new price update from oracle, proceed as normal
+        if (assetPrice == newPrice) {
+            return;
+        }
+
+        int256 percentageChange;
+        int256 valueChange = 0;
+        // Long gains
+        if (newPrice > assetPrice) {
+            percentageChange = (newPrice - assetPrice) / assetPrice;
+            if (getShortBeta() == 1) {
+                valueChange = shortValue * percentageChange;
+            } else {
+                valueChange = longValue * percentageChange;
+            }
+            longValue = longValue + valueChange;
+            shortValue = shortValue - valueChange; // NB Check for going below zero and system instability
+        } else {
+            percentageChange = (assetPrice - newPrice) / assetPrice;
+            if (getShortBeta() == 1) {
+                valueChange = shortValue * percentageChange;
+            } else {
+                valueChange = longValue * percentageChange;
+            }
+            longValue = longValue - valueChange;
+            shortValue = shortValue + valueChange;
+        }
+
+        // Update price of tokens
+        // careful if total supply is zero intitally.
+        //longTokenPrice = longValue / longTokens.totalSupply();
+        //shortTokenPrice = shortValue / shortTokens.totalSupply();
+        assetPrice = newPrice;
+
+        // Build such that interest gets accredited to each side in 50/50 or other split
+        // Before every contract action (unless actions happen within 5minutes I would say?)
+        // Calculate
+    }
+
+    /**
+     * Create a long position
+     */
+    function mintLong(uint256 amount) external refreshSystemState {
+        aaveLendingContract = IAaveLendingPool(provider.getLendingPool());
+        aaveLendingContractCore = provider.getLendingPoolCore();
+
+        daiContract.transferFrom(msg.sender, address(this), amount);
+        daiContract.approve(aaveLendingContractCore, amount);
+        aaveLendingContract.deposit(address(daiContract), amount, 30);
+
+        totalValueLocked = totalValueLocked.add(amount);
+        // Mint correct amount of long tokens based on current price
+        // add the value to the long position
+        // longTokens.mint(msg.sender, amount);
     }
 }
---
+++ contracts/ShortCoins.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.8;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/presets/ERC20PresetMinterPauser.sol";
+
+contract ShortCoins is ERC20PresetMinterPauserUpgradeSafe {
+    // function initialize(string memory name, string memory symbol)
+    //     public
+    //     override
+    // {
+    //     __ERC20PresetMinterPauser_init(name, symbol);
+    // }
+}
