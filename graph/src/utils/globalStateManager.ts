f76ead36b5c2ae18090a5103c3f57c25bdb304c0
jonjon <jonjonclark@gmail.com>
2021-02-09 17:27:04 +0200

    Graph all working with new schema.

--- graph/src/utils/globalStateManager.ts
+++ graph/src/utils/globalStateManager.ts
@@ -8,17 +8,19 @@ import { BigInt, Address, Bytes, log, ethereum } from "@graphprotocol/graph-ts";
 import { ZERO } from "../CONSTANTS";
 
 export function getOrCreateLatestSystemState(
+  marketIndex: BigInt,
   latestStateChangeCounter: BigInt,
   event: ethereum.Event
 ): SystemState | null {
   let systemStateId = latestStateChangeCounter.toString();
-  let latestSystemState = SystemState.load(systemStateId);
+  let marketIndexId = marketIndex.toString();
+  let latestSystemState = SystemState.load(marketIndexId + "-" + systemStateId);
   if (latestSystemState == null) {
-    latestSystemState = new SystemState(systemStateId);
+    latestSystemState = new SystemState(marketIndexId + "-" + systemStateId);
     latestSystemState.timestamp = event.block.timestamp;
     latestSystemState.txHash = event.transaction.hash;
     latestSystemState.blockNumber = event.block.number;
-    latestSystemState.marketIndex = ZERO;
+    latestSystemState.marketIndex = marketIndex;
     latestSystemState.syntheticPrice = ZERO;
     latestSystemState.longTokenPrice = ZERO;
     latestSystemState.shortTokenPrice = ZERO;
