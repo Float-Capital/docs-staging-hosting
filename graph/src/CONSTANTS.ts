1001ca796c9d52a207f16b432eb68de8a04ec96d
Jason <Jason@wildcards.world>
2021-02-16 11:24:51 +0200

    Update graph to log deployed contract addresses

--- graph/src/CONSTANTS.ts
+++ graph/src/CONSTANTS.ts
@@ -1,6 +1,10 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 export const GLOBAL_STATE_ID = "globalState";
+export const ORACLE_AGREGATOR_ID = "oracleAgregator";
+export const YIELD_MANAGER_ID = "yieldManager";
+export const STAKER_ID = "staker";
+export const TOKEN_FACTORY_ID = "tokenFactory";
 
 export let ZERO = BigInt.fromI32(0);
 export let BILLION = BigInt.fromI32(1000000000); // 10^9
