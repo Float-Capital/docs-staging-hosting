f4f94165c1bd91f0b3199377fd24b28ab8e19dab
jonjon <jonjonclark@gmail.com>
2021-02-17 13:54:47 +0200

    Fixed lots of stuff. everywhere. Can now read events for the staker contract in the graph.

--- scripts/env.js
+++ scripts/env.js
@@ -1,17 +1,19 @@
 module.exports = {
-    openZeppelinDir: "./contracts/.openzeppelin",
-    openZeppelinToYaml: {
-        "dev-97.json" :  "subgraph.binance-test.yaml",
-        "goerli.json" :  "subgraph.yaml",
-        "dev-321.json": "subgraph.ganache.yaml"
-    },
-    networksToOpenZeppelin: {
-        "binanceTest" : "dev-97.json",
-        "goerli": "goerli.json"
-    },
-    implementationVarsToProxies: {
-        "LONGSHORT_IMPLEMENTATION": "float-capital/OracleAgregator",
-        "ORACLE_MANAGER_IMPLEMENTATION": "float-capital/YieldManager",
-        "YIELD_MANAGER_IMPLEMENTATION": "float-capital/LongShort" 
-    }   
-}
+  openZeppelinDir: "./contracts/.openzeppelin",
+  openZeppelinToYaml: {
+    "dev-97.json": "subgraph.binance-test.yaml",
+    "goerli.json": "subgraph.yaml",
+    "dev-321.json": "subgraph.ganache.yaml",
+  },
+  networksToOpenZeppelin: {
+    binanceTest: "dev-97.json",
+    goerli: "goerli.json",
+    ganache: "dev-321.json",
+  },
+  implementationVarsToProxies: {
+    LONGSHORT_IMPLEMENTATION: "float-capital/OracleAgregator",
+    ORACLE_MANAGER_IMPLEMENTATION: "float-capital/YieldManager",
+    YIELD_MANAGER_IMPLEMENTATION: "float-capital/LongShort",
+    STAKER_IMPLEMENTATION: "float-capital/Staker",
+  },
+};
--- scripts/graph.js
+++ scripts/graph.js
@@ -2,49 +2,75 @@ const fs = require("fs");
 const path = require("path");
 const env = require("./env");
 
+// PASS IN THE NETWORKS AS ARGS e.g. node scripts/verify.js goerli binanceTest
+// if none passed verifies for all the networks it's configured of
+const args = process.argv.splice(2);
+let openzeppelin;
+if (args.length > 0) {
+  openzeppelin = args.map((x) => env.networksToOpenZeppelin[x]);
+} else {
+  openzeppelin = Object.keys(env.openZeppelinToYaml);
+}
 
-const addressKey = "<long-short-address>";
+const longShortAddressKey = "<long-short-address>";
+const stakerAddressKey = "<staker-address>";
 
 const preserveKeys = [
-    "<network>",
-    "<start-block>"
-]
+  "<network>",
+  "<start-block>",
+  "<staker-network>",
+  "<staker-start-block>",
+];
 
 const prefix = env.openZeppelinDir;
 const graphPath = "./graph";
 
 const templateName = "subgraph.template.yaml";
 
-const addAddress = (template, input) => {
-    const address = `"${(JSON.parse(input)).contracts.LongShort.address}"`;
-    return template.replace(addressKey, address);
-}
+const addAddressLongShort = (template, input) => {
+  const address = `"${
+    JSON.parse(input).proxies["float-capital/LongShort"][0].address
+  }"`;
+  return template.replace(longShortAddressKey, address);
+};
+
+const addAddressStaker = (template, input) => {
+  const address = `"${
+    JSON.parse(input).proxies["float-capital/Staker"][0].address
+  }"`;
+  return template.replace(stakerAddressKey, address);
+};
 
 const preserveValues = (template, output) => {
-    templateLines = template.split('\n');
-    outputLines = output.split('\n');
-    preserveKeys.forEach(p => {
-        const i = templateLines.findIndex((l) => l.includes(p));
-        if(i != -1){
-            templateVal = outputLines[i].split(":")[1].trim();
-            template = template.replace(p, templateVal);
-        }
-    });
-    return template;
-}
-
-Object.keys(env.openZeppelinToYaml).forEach(file => {
-    let input, output, template;
-    try{
-        input = fs.readFileSync(path.join(prefix, file)).toString();
-        template = fs.readFileSync(path.join(graphPath, templateName)).toString();
-        output = fs.readFileSync(path.join(graphPath, env.openZeppelinToYaml[file])).toString();
-    }catch(e){
-        console.log(e);
-        return;
+  templateLines = template.split("\n");
+  outputLines = output.split("\n");
+  preserveKeys.forEach((p) => {
+    const i = templateLines.findIndex((l) => l.includes(p));
+    if (i != -1) {
+      templateVal = outputLines[i].split(":")[1].trim();
+      template = template.replace(p, templateVal);
     }
-    template = addAddress(template, input);
-    template = preserveValues(template, output);
-    fs.writeFileSync(path.join(graphPath, env.openZeppelinToYaml[file]), template); 
-})
+  });
+  return template;
+};
 
+openzeppelin.forEach((file) => {
+  let input, output, template;
+  try {
+    input = fs.readFileSync(path.join(prefix, file)).toString();
+    template = fs.readFileSync(path.join(graphPath, templateName)).toString();
+    output = fs
+      .readFileSync(path.join(graphPath, env.openZeppelinToYaml[file]))
+      .toString();
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+  template = addAddressLongShort(template, input);
+  template = addAddressStaker(template, input);
+  template = preserveValues(template, output);
+  fs.writeFileSync(
+    path.join(graphPath, env.openZeppelinToYaml[file]),
+    template
+  );
+});
