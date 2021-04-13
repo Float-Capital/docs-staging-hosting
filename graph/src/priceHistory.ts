d881877966bc5a9cbeccc882dc494351412bb445
Jason <Jason@wildcards.world>
2021-04-13 22:22:56 +0200

    Complete 5 min price history updates

--- graph/src/priceHistory.ts
+++ graph/src/priceHistory.ts
@@ -6,7 +6,8 @@ import {
   FiveMinPrice,
 } from "../generated-price-history/schema";
 import { Address, BigInt, ethereum, log } from "@graphprotocol/graph-ts";
-import { ONE, ZERO } from "./CONSTANTS";
+import { ONE, FIVE_MINUTES_IN_SECONDS } from "./CONSTANTS";
+
 export function getOrCreateGlobalState(): Global {
   let globalState = Global.load("GLOBAL");
   if (globalState == null) {
@@ -25,6 +26,7 @@ export function createOracle(
   initialFiveMinPrice: FiveMinPrice
 ): Oracle {
   let oracle = new Oracle(oracleAddress.toHex());
+  oracle.address = oracleAddress;
   oracle.marketIndex = marketIndex;
   oracle.marketName = marketName;
   oracle.marketSymbol = marketSymbol;
@@ -35,39 +37,61 @@ export function createOracle(
   return oracle as Oracle;
 }
 
-export function handleSyntheticTokenCreated(
-  event: SyntheticTokenCreated
-): void {
-  let marketIndex = event.params.marketIndex;
+export function createFiveMinPrice(
+  timestamp: BigInt,
+  oracleAddressStr: string,
+  currentOraclePrice: BigInt
+): FiveMinPrice {
+  let intervalIndex = timestamp.mod(FIVE_MINUTES_IN_SECONDS);
+  let fiveMinPrice = new FiveMinPrice(
+    oracleAddressStr + "-" + intervalIndex.toString()
+  );
 
-  let oracleAddress = event.params.oracleAddress;
-  let oracleAddressStr = oracleAddress.toHex();
-  let marketName = event.params.name;
-  let marketSymbol = event.params.symbol;
+  fiveMinPrice.intervalIndex = intervalIndex;
+  fiveMinPrice.startPrice = currentOraclePrice;
+  fiveMinPrice.endPrice = currentOraclePrice;
+  fiveMinPrice.averagePrice = currentOraclePrice;
+  fiveMinPrice.intervalComplete = false;
+  fiveMinPrice.numberOfBlocksInInterval = ONE;
+  fiveMinPrice.oracle = oracleAddressStr;
 
-  let marketIndexString = marketIndex.toString();
+  return fiveMinPrice as FiveMinPrice;
+}
 
-  let state = getOrCreateGlobalState();
-
+function getLatestOraclePrice(oracleAddress: Address): BigInt {
   let oracleInstance = OracleManager.bind(oracleAddress);
 
   let latestPriceQuery = oracleInstance.try_updatePrice();
   if (latestPriceQuery.reverted) {
     log.warning(
       "Unable to get latest price from oracle manager at {}. Please check the ABI is correct",
-      [oracleAddressStr]
+      [oracleAddress.toHex()]
     );
   }
 
-  let currentOraclePrice = latestPriceQuery.value;
+  return latestPriceQuery.value;
+}
 
-  let fiveMinPrice = new FiveMinPrice(oracleAddressStr + "-0");
+export function handleSyntheticTokenCreated(
+  event: SyntheticTokenCreated
+): void {
+  let marketIndex = event.params.marketIndex;
+  let timestamp = event.block.timestamp;
 
-  fiveMinPrice.startPrice = currentOraclePrice;
-  fiveMinPrice.endPrice = currentOraclePrice;
-  fiveMinPrice.averagePrice = currentOraclePrice;
-  fiveMinPrice.intervalComplete = false;
-  fiveMinPrice.numberOfBlocksInInterval = ONE;
+  let oracleAddress = event.params.oracleAddress;
+  let oracleAddressStr = oracleAddress.toHex();
+  let marketName = event.params.name;
+  let marketSymbol = event.params.symbol;
+
+  let state = getOrCreateGlobalState();
+
+  let currentOraclePrice = getLatestOraclePrice(oracleAddress);
+
+  let fiveMinPrice = createFiveMinPrice(
+    timestamp,
+    oracleAddressStr,
+    currentOraclePrice
+  );
 
   let oracle = createOracle(
     oracleAddress,
@@ -85,5 +109,53 @@ export function handleSyntheticTokenCreated(
 }
 
 export function handleBlock(block: ethereum.Block): void {
-  log.warning("New block", []);
+  let state = getOrCreateGlobalState();
+  let timestamp = block.timestamp;
+
+  let allOracles = state.oracles;
+  for (let i = 0; i < allOracles.length; ++i) {
+    let curentOracleId = allOracles[i];
+    let oracle = Oracle.load(curentOracleId);
+    if (oracle == null) {
+      log.critical("Oracle with address {} is undefined", [curentOracleId]);
+    }
+
+    let latestFiveMinPrice = FiveMinPrice.load(oracle.latestFiveMinPrice);
+    if (latestFiveMinPrice == null) {
+      log.critical("The latest 5 minute price is undefined", [curentOracleId]);
+    }
+
+    let currentOraclePrice = getLatestOraclePrice(oracle.address as Address);
+
+    oracle.latestPrice = currentOraclePrice;
+
+    // FIVE_MINUTES_IN_SECONDS;
+    let currentPriceIndex = timestamp.mod(FIVE_MINUTES_IN_SECONDS);
+    if (currentPriceIndex.equals(latestFiveMinPrice.intervalIndex)) {
+      let newNumberOfBlocksInInterval = latestFiveMinPrice.numberOfBlocksInInterval.plus(
+        ONE
+      );
+      latestFiveMinPrice.endPrice = currentOraclePrice;
+      latestFiveMinPrice.averagePrice = latestFiveMinPrice.averagePrice
+        .times(latestFiveMinPrice.numberOfBlocksInInterval)
+        .plus(currentOraclePrice)
+        .div(newNumberOfBlocksInInterval);
+      latestFiveMinPrice.numberOfBlocksInInterval = newNumberOfBlocksInInterval;
+      latestFiveMinPrice.intervalComplete = false;
+
+      latestFiveMinPrice.save();
+    } else {
+      let fiveMinPrice = createFiveMinPrice(
+        timestamp,
+        oracle.id,
+        currentOraclePrice
+      );
+      oracle.fiveMinPrices = oracle.fiveMinPrices.concat([fiveMinPrice.id]);
+      oracle.latestFiveMinPrice = fiveMinPrice.id;
+
+      fiveMinPrice.save();
+    }
+
+    oracle.save();
+  }
 }
