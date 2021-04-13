a3743fe7457b03a165cd4b6b97f9e7047543b894
Jason <Jason@wildcards.world>
2021-04-13 17:20:53 +0200

    Basic market creation schema in place

--- graph/src/priceHistory.ts
+++ graph/src/priceHistory.ts
@@ -1,24 +1,89 @@
-import { SyntheticTokenCreated } from "../generated/LongShort/LongShort";
-import { log } from "@graphprotocol/graph-ts";
+import { SyntheticTokenCreated } from "../generated-price-history/LongShort/LongShort";
+import { OracleManager } from "../generated-price-history/templates/OracleManager/OracleManager";
+import {
+  Global,
+  Oracle,
+  FiveMinPrice,
+} from "../generated-price-history/schema";
+import { Address, BigInt, ethereum, log } from "@graphprotocol/graph-ts";
+import { ONE, ZERO } from "./CONSTANTS";
+export function getOrCreateGlobalState(): Global {
+  let globalState = Global.load("GLOBAL");
+  if (globalState == null) {
+    globalState = new Global("GLOBAL");
+    globalState.oracles = [];
+  }
+
+  return globalState as Global;
+}
+
+export function createOracle(
+  oracleAddress: Address,
+  marketIndex: BigInt,
+  marketName: string,
+  marketSymbol: string,
+  initialFiveMinPrice: FiveMinPrice
+): Oracle {
+  let oracle = new Oracle(oracleAddress.toHex());
+  oracle.marketIndex = marketIndex;
+  oracle.marketName = marketName;
+  oracle.marketSymbol = marketSymbol;
+  oracle.latestPrice = initialFiveMinPrice.startPrice;
+  oracle.fiveMinPrices = [];
+  oracle.latestFiveMinPrice = initialFiveMinPrice.id;
+
+  return oracle as Oracle;
+}
 
 export function handleSyntheticTokenCreated(
   event: SyntheticTokenCreated
 ): void {
-  let txHash = event.transaction.hash;
-  let blockNumber = event.block.number;
-  let timestamp = event.block.timestamp;
-
   let marketIndex = event.params.marketIndex;
-  let longTokenAddress = event.params.longTokenAddress;
-  let shortTokenAddress = event.params.shortTokenAddress;
-  let collateralTokenAddress = event.params.fundToken;
-  let initialAssetPrice = event.params.assetPrice;
 
   let oracleAddress = event.params.oracleAddress;
-  let syntheticName = event.params.name;
-  let syntheticSymbol = event.params.symbol;
+  let oracleAddressStr = oracleAddress.toHex();
+  let marketName = event.params.name;
+  let marketSymbol = event.params.symbol;
 
   let marketIndexString = marketIndex.toString();
 
-  log.warning("Added market - ${}", [marketIndexString]);
+  let state = getOrCreateGlobalState();
+
+  let oracleInstance = OracleManager.bind(oracleAddress);
+
+  let latestPriceQuery = oracleInstance.try_updatePrice();
+  if (latestPriceQuery.reverted) {
+    log.warning(
+      "Unable to get latest price from oracle manager at {}. Please check the ABI is correct",
+      [oracleAddressStr]
+    );
+  }
+
+  let currentOraclePrice = latestPriceQuery.value;
+
+  let fiveMinPrice = new FiveMinPrice(oracleAddressStr + "-0");
+
+  fiveMinPrice.startPrice = currentOraclePrice;
+  fiveMinPrice.endPrice = currentOraclePrice;
+  fiveMinPrice.averagePrice = currentOraclePrice;
+  fiveMinPrice.intervalComplete = false;
+  fiveMinPrice.numberOfBlocksInInterval = ONE;
+
+  let oracle = createOracle(
+    oracleAddress,
+    marketIndex,
+    marketName,
+    marketSymbol,
+    fiveMinPrice
+  );
+
+  state.oracles = state.oracles.concat([oracle.id]);
+
+  state.save();
+  oracle.save();
+  fiveMinPrice.save();
+}
+
+export function handleBlock(block: ethereum.Block): void {
+  log.warning("New block", []);
 }
