6b628e16139d4caa2381cce7dc81487be790e156
denhampreen <denhampreen@gmail.com>
2021-05-10 15:42:55 +0300

    make discord link in coming soon banner more obvious

--- dapp/src/components/ComingSoon.js
+++ dapp/src/components/ComingSoon.js
@@ -8,7 +8,7 @@ function ComingSoon(Props) {
   return React.createElement("div", {
               className: "bg-primary text-center text-sm text-white p-1 mb-2 "
             }, "🏗 The protocol is under active development, join our ", React.createElement("a", {
-                  className: "hover:bg-white hover:text-primary py-1 ",
+                  className: "bg-white hover:bg-primary-light text-primary hover:text-white py-1 font-bold",
                   href: Config.discordInviteLink,
                   rel: "noopener noreferrer",
                   target: "_blank"
--- dapp/src/components/ComingSoon.res
+++ dapp/src/components/ComingSoon.res
@@ -3,7 +3,7 @@ let make = () => {
   <div className="bg-primary text-center text-sm text-white p-1 mb-2 ">
     {`🏗 The protocol is under active development, join our `->React.string}
     <a
-      className="hover:bg-white hover:text-primary py-1 "
+      className="bg-white hover:bg-primary-light text-primary hover:text-white py-1 font-bold"
       href=Config.discordInviteLink
       target="_blank"
       rel="noopener noreferrer">
