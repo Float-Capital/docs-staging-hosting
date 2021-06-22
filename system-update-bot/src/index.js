1d032f1c44973d6bdfcc32f8385040939ee84233
Jason <Jason@wildcards.world>
2021-06-22 23:09:24 +0300

    Update price update script

--- system-update-bot/src/index.js
+++ system-update-bot/src/index.js
@@ -1,4 +1,3 @@
-var cron = require("node-cron");
 var secretsManager = require("../secretsManager.js");
 var ethers = require("ethers");
 
@@ -6,17 +5,31 @@ const longShortContractAddress = "0x3Fe666e021714eaF51e061D8efFCc6fA4b390DC0";
 const longShortAbi = [
   "function getMarketSplit(uint32 marketIndex, uint256 amount) view returns (uint256 longAmount, uint256 shortAmount) @100000",
   "function _updateSystemState(uint32 marketIndex) external @400000", //TODO: Optimise the gas here
-  "function _updateSystemStateMulti(uint32[] calldata marketIndexes) external @900000", // TODO: remove _ in latest version of contracts
+  "function _updateSystemStateMulti(uint32[] calldata marketIndexes) external @1800000", // TODO: remove _ in latest version of contracts
 ];
 
-let abi = cron.schedule("*/15 * * * * *", async () => {
-  let provider = await new ethers.providers.JsonRpcProvider(
+
+let provider;
+
+let wallet;
+
+const setup = async () => {
+  console.log({ secretsManager })
+  provider = await new ethers.providers.JsonRpcProvider(
     secretsManager.providerUrl
   );
 
-  let wallet = await new ethers.Wallet.fromMnemonic(secretsManager.mnemonic);
+  wallet = await new ethers.Wallet.fromMnemonic(secretsManager.mnemonic);
 
   wallet = wallet.connect(provider);
+}
+
+let updateCounter = 0;
+
+const defaultOptions = { gasPrice: 1000000000 };
+
+const runUpdateSystemStateMulti = async () => {
+  console.log("running update", ++updateCounter)
 
   let walletBalance = await wallet.getBalance();
 
@@ -29,7 +42,7 @@ let abi = cron.schedule("*/15 * * * * *", async () => {
   );
 
   try {
-    let update = await contract.functions._updateSystemStateMulti([1, 2, 3]);
+    let update = await contract.functions._updateSystemStateMulti([1, 2, 3], defaultOptions);
     console.log(JSON.stringify(update));
   } catch (e) {
     console.log("ERROR");
@@ -40,4 +53,9 @@ let abi = cron.schedule("*/15 * * * * *", async () => {
   walletBalance = await wallet.getBalance();
 
   console.log("Matic balance post contract call: ", walletBalance.toString());
-});
+}
+
+setup().then(() => {
+  runUpdateSystemStateMulti()
+  setInterval(runUpdateSystemStateMulti, 15000)
+})
