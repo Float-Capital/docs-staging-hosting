42318e9f7d4a5b5686e54dfd2c0e8249598e519d
denhampreen <denhampreen@gmail.com>
2021-06-22 11:35:01 +0200

    basic bot to update system state

---
+++ system-update-bot/secretsManager.example.js
@@ -0,0 +1,7 @@
+const mnemonic = "";
+const providerUrl = "https://rpc-mumbai.matic.today";
+
+module.exports = {
+  mnemonic,
+  providerUrl,
+};
---
+++ system-update-bot/src/index.js
@@ -0,0 +1,43 @@
+var cron = require("node-cron");
+var secretsManager = require("../secretsManager.js");
+var ethers = require("ethers");
+
+const longShortContractAddress = "0x3Fe666e021714eaF51e061D8efFCc6fA4b390DC0";
+const longShortAbi = [
+  "function getMarketSplit(uint32 marketIndex, uint256 amount) view returns (uint256 longAmount, uint256 shortAmount) @100000",
+  "function _updateSystemState(uint32 marketIndex) external @400000", //TODO: Optimise the gas here
+  "function _updateSystemStateMulti(uint32[] calldata marketIndexes) external @50000", // TODO: remove _ in latest version of contracts
+];
+
+let abi = cron.schedule("*/15 * * * * *", async () => {
+  let provider = await new ethers.providers.JsonRpcProvider(
+    secretsManager.providerUrl
+  );
+
+  let wallet = await new ethers.Wallet.fromMnemonic(secretsManager.mnemonic);
+
+  wallet = wallet.connect(provider);
+
+  let walletBalance = await wallet.getBalance();
+
+  console.log("Matic balance pre contract call: ", walletBalance.toString());
+
+  let contract = new ethers.Contract(
+    longShortContractAddress,
+    longShortAbi,
+    wallet
+  );
+
+  try {
+    let update = await contract.functions._updateSystemStateMulti([1, 2, 3]);
+    console.log(JSON.stringify(update));
+  } catch (e) {
+    console.log("ERROR");
+    console.log("-------------------");
+    console.log(e);
+  }
+
+  walletBalance = await wallet.getBalance();
+
+  console.log("Matic balance post contract call: ", walletBalance.toString());
+});
