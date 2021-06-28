d75014923659a165556ed78dfef9cf3503b30712
Jason <Jason@wildcards.world>
2021-06-28 20:34:38 +0200

    Remove the force-recreate flag

--- graph/scripts/updateSelfHostedGraph.sh
+++ graph/scripts/updateSelfHostedGraph.sh
@@ -6,11 +6,11 @@ cp -r abis ../graph/abis-copy
 
 cd ../graph/scripts
 
-docker-compose -f docker-compose.mumbai.selfhosted.yaml up --force-recreate --build -d
+docker-compose -f docker-compose.mumbai.selfhosted.yaml up --build -d
 docker image prune -f
 
 # Give it time start everything...
-sleep 60
+sleep 120
 
 docker exec -ti graph-deployer sh -c "yarn graph-command create float-capital/float-capital --node http://graph-node:8020"
 docker exec -ti graph-deployer sh -c "yarn graph-command create float-capital/float-oracle-prices --node http://graph-node:8020"
