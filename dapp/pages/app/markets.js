dd7983c1cd7db1544d52029219bdf4fcf9312907
denhampreen <denhampreen@gmail.com>
2021-07-09 18:46:49 +0200

    move and relink pages

--- dapp/pages/app/markets.js
+++ dapp/pages/app/markets.js
@@ -1,11 +1,11 @@
-import Markets from "src/pages/Markets";
+import IndexRes from "src/pages/Home.js";
 import HtmlHeader from "src/components/HtmlHeader.js";
 
-export default function MarketsPage(props) {
+export default function Index(props) {
   return (
     <div>
-      <HtmlHeader page="Markets"></HtmlHeader>
-      <Markets {...props} />
+      <HtmlHeader page="Home"></HtmlHeader>
+      <IndexRes {...props} />
     </div>
   );
 }
