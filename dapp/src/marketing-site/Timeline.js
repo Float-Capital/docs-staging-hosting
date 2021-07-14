6f6bbea1eda315ccf68f0cfe70df0c18d24d33ee
denhampreen <denhampreen@gmail.com>
2021-07-14 09:58:32 +0200

    timeline points

--- dapp/src/marketing-site/Timeline.js
+++ dapp/src/marketing-site/Timeline.js
@@ -7,34 +7,42 @@ function Timeline(Props) {
   return React.createElement("div", {
               className: "container"
             }, React.createElement("div", {
-                  className: "flex flex-row"
+                  className: "flex flex-row flex-wrap"
                 }, React.createElement("div", {
-                      className: "w-1/2"
+                      className: "w-full md:w-1/2 mx-4 md:mx-0 order"
                     }, React.createElement("div", {
-                          className: "bg-white p-4 rounded-md rounded-br-none my-4 shadow-md inline-block"
+                          className: "bg-white p-4 rounded-md md:rounded-br-none my-4 shadow-md inline-block w-full md:w-auto relative"
                         }, React.createElement("h3", {
                               className: "font-semibold text-lg mb-1"
-                            }, "Q1 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "Protocol mechanism researched & designed"), React.createElement("li", undefined, "Core suite of smart contracts implemented"), React.createElement("li", undefined, "MVP UI created"), React.createElement("li", undefined, "Functional testnet deployment to BSC")))), React.createElement("div", {
-                      className: "w-1/2"
+                            }, "Q1 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "Protocol mechanism researched & designed"), React.createElement("li", undefined, "Core suite of smart contracts implemented"), React.createElement("li", undefined, "MVP UI created"), React.createElement("li", undefined, "Functional testnet deployment to BSC")), React.createElement("div", {
+                              className: "bg-white h-4 w-4 border rounded-full absolute -right-2 -bottom-6"
+                            }))), React.createElement("div", {
+                      className: "w-full md:w-1/2 mx-4 md:mx-0"
                     }, React.createElement("div", {
-                          className: "bg-white p-4 rounded-md rounded-br-none my-4 shadow-md  inline-block"
+                          className: "bg-white p-4 rounded-md md:rounded-br-none my-4 shadow-md  inline-block w-full md:w-auto relative"
                         }, React.createElement("h3", {
                               className: "font-semibold text-lg mb-1"
-                            }, "Q3 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "Finalize protocol design"), React.createElement("li", undefined, "Audit smart contracts"), React.createElement("li", undefined, "Build dream team"), React.createElement("li", undefined, "UI/UX refinement"))))), React.createElement("hr", undefined), React.createElement("div", {
+                            }, "Q3 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "Finalize protocol design"), React.createElement("li", undefined, "Audit smart contracts"), React.createElement("li", undefined, "Build dream team"), React.createElement("li", undefined, "UI/UX refinement")), React.createElement("div", {
+                              className: "bg-white h-4 w-4 border rounded-full absolute -right-2 -bottom-6"
+                            })))), React.createElement("hr", undefined), React.createElement("div", {
                   className: "flex flex-row"
                 }, React.createElement("div", {
                       className: "w-1/4"
                     }), React.createElement("div", {
-                      className: "bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md  inline-block"
+                      className: "bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md  inline-block w-full md:w-auto relative"
                     }, React.createElement("h3", {
                           className: "font-semibold text-lg mb-1"
-                        }, "Q2 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "UI Design and iteration"), React.createElement("li", undefined, "Move from BSC to Polygon"), React.createElement("li", undefined, "Integrate Chainlink and Aave"), React.createElement("li", undefined, "Raise seed funding"))), React.createElement("div", {
+                        }, "Q2 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "UI Design and iteration"), React.createElement("li", undefined, "Move from BSC to Polygon"), React.createElement("li", undefined, "Integrate Chainlink and Aave"), React.createElement("li", undefined, "Raise seed funding")), React.createElement("div", {
+                          className: "bg-white h-4 w-4 border rounded-full absolute -right-2 -top-6"
+                        })), React.createElement("div", {
                       className: "w-1/4"
                     }), React.createElement("div", {
-                      className: "bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md  inline-block"
+                      className: "bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md ml-auto inline-block  w-full md:w-auto relative"
                     }, React.createElement("h3", {
                           className: "font-semibold text-lg mb-1"
-                        }, "Q4 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "Launch new markets"), React.createElement("li", undefined, "Pull the rug"), React.createElement("li", undefined, "Raise Series A"), React.createElement("li", undefined, "Implement Governance module")))));
+                        }, "Q4 2021"), React.createElement("ul", undefined, React.createElement("li", undefined, "Launch new markets"), React.createElement("li", undefined, "Support offchain markets"), React.createElement("li", undefined, "Raise Series A"), React.createElement("li", undefined, "Implement Governance module")), React.createElement("div", {
+                          className: "bg-white h-4 w-4 border rounded-full absolute -right-2 -top-6"
+                        }))));
 }
 
 var make = Timeline;
--- dapp/src/marketing-site/Timeline.res
+++ dapp/src/marketing-site/Timeline.res
@@ -1,9 +1,10 @@
 @react.component
 let make = () => {
   <div className="container">
-    <div className="flex flex-row">
-      <div className="w-1/2">
-        <div className="bg-white p-4 rounded-md rounded-br-none my-4 shadow-md inline-block">
+    <div className="flex flex-row flex-wrap">
+      <div className="w-full md:w-1/2 mx-4 md:mx-0 order">
+        <div
+          className="bg-white p-4 rounded-md md:rounded-br-none my-4 shadow-md inline-block w-full md:w-auto relative">
           <h3 className="font-semibold text-lg mb-1"> {"Q1 2021"->React.string} </h3>
           <ul>
             <li> {"Protocol mechanism researched & designed"->React.string} </li>
@@ -11,10 +12,12 @@ let make = () => {
             <li> {"MVP UI created"->React.string} </li>
             <li> {"Functional testnet deployment to BSC"->React.string} </li>
           </ul>
+          <div className="bg-white h-4 w-4 border rounded-full absolute -right-2 -bottom-6" />
         </div>
       </div>
-      <div className="w-1/2">
-        <div className="bg-white p-4 rounded-md rounded-br-none my-4 shadow-md  inline-block">
+      <div className="w-full md:w-1/2 mx-4 md:mx-0">
+        <div
+          className="bg-white p-4 rounded-md md:rounded-br-none my-4 shadow-md  inline-block w-full md:w-auto relative">
           <h3 className="font-semibold text-lg mb-1"> {"Q3 2021"->React.string} </h3>
           <ul>
             <li> {"Finalize protocol design"->React.string} </li>
@@ -22,13 +25,15 @@ let make = () => {
             <li> {"Build dream team"->React.string} </li>
             <li> {"UI/UX refinement"->React.string} </li>
           </ul>
+          <div className="bg-white h-4 w-4 border rounded-full absolute -right-2 -bottom-6" />
         </div>
       </div>
     </div>
     <hr />
     <div className="flex flex-row">
       <div className="w-1/4" />
-      <div className="bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md  inline-block">
+      <div
+        className="bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md  inline-block w-full md:w-auto relative">
         <h3 className="font-semibold text-lg mb-1"> {"Q2 2021"->React.string} </h3>
         <ul>
           <li> {"UI Design and iteration"->React.string} </li>
@@ -36,16 +41,19 @@ let make = () => {
           <li> {"Integrate Chainlink and Aave"->React.string} </li>
           <li> {"Raise seed funding"->React.string} </li>
         </ul>
+        <div className="bg-white h-4 w-4 border rounded-full absolute -right-2 -top-6" />
       </div>
       <div className="w-1/4" />
-      <div className="bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md  inline-block">
+      <div
+        className="bg-white p-4 rounded-md rounded-tr-none my-4 shadow-md ml-auto inline-block  w-full md:w-auto relative">
         <h3 className="font-semibold text-lg mb-1"> {"Q4 2021"->React.string} </h3>
         <ul>
           <li> {"Launch new markets"->React.string} </li>
-          <li> {"Pull the rug"->React.string} </li>
+          <li> {"Support offchain markets"->React.string} </li>
           <li> {"Raise Series A"->React.string} </li>
           <li> {"Implement Governance module"->React.string} </li>
         </ul>
+        <div className="bg-white h-4 w-4 border rounded-full absolute -right-2 -top-6" />
       </div>
     </div>
   </div>
