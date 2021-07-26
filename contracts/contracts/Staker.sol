1aebb250828515f3528f703e9abd71bd9ba23944
Jason <Jason@wildcards.world>
2021-07-26 07:59:35 +0200

    Basic solution for exponentiation overflow

--- contracts/contracts/Staker.sol
+++ contracts/contracts/Staker.sol
@@ -17,6 +17,9 @@ contract Staker is IStaker, Initializable {
 
   // Fixed-precision constants
   uint256 public constant FLOAT_ISSUANCE_FIXED_DECIMAL = 1e42;
+  // 2^52 ~= 4.5e15
+  // With an exponent of 5, the largest total liquidity possible in market (to avoid integer overflow on exponentiation) is ~10^31 or 10 Trillion (10^13)
+  uint256 public constant safeExponentBitShifting = 52;
 
   // Global state
   address public admin;
@@ -229,7 +232,8 @@ contract Staker is IStaker, Initializable {
     uint256 _balanceIncentiveCurveExponent
   ) internal virtual {
     require(
-      _balanceIncentiveCurveExponent > 0 && _balanceIncentiveCurveExponent < 10000,
+      // The exponent has to be less than 5 in these versions of the contracts.
+      _balanceIncentiveCurveExponent > 0 && _balanceIncentiveCurveExponent < 6,
       "balanceIncentiveCurveExponent out of bounds"
     );
 
@@ -358,24 +362,6 @@ contract Staker is IStaker, Initializable {
     }
   }
 
-  // TODO: turn this into a library.
-  //       make this a simple lookup table with appropriate aproximation
-  function getRequiredAmountOfBitShiftForSafeExponentiation(uint256 number, uint256 exponent)
-    internal
-    pure
-    virtual
-    returns (uint256 amountOfBitShiftRequired)
-  {
-    uint256 targetMaxNumberSizeBinaryDigits = 257 / exponent;
-
-    // Note this can be optimised, this gets a quick easy to compute safe upper bound, not the actuall upper bound.
-    uint256 targetMaxNumber = 2**targetMaxNumberSizeBinaryDigits;
-
-    while (number >> amountOfBitShiftRequired > targetMaxNumber) {
-      ++amountOfBitShiftRequired;
-    }
-  }
-
   /*
    * Computes the current 'r' value, i.e. the number of float tokens a user
    * earns per second for every longshort token they've staked. The returned
@@ -405,11 +391,6 @@ contract Staker is IStaker, Initializable {
     int256 equilibriumOffsetMarketScaled = (balanceIncentiveCurveEquilibriumOffset[marketIndex] *
       int256(totalLocked)) / 1e18;
 
-    uint256 requiredBitShifting = getRequiredAmountOfBitShiftForSafeExponentiation(
-      totalLocked,
-      balanceIncentiveCurveExponent[marketIndex]
-    );
-
     // Float is scaled by the percentage of the total market value held in
     // the opposite position. This incentivises users to stake on the
     // weaker position.
@@ -421,9 +402,9 @@ contract Staker is IStaker, Initializable {
       }
 
       uint256 numerator = (uint256(int256(shortValue) - equilibriumOffsetMarketScaled) >>
-        (requiredBitShifting - 1))**balanceIncentiveCurveExponent[marketIndex];
+        (safeExponentBitShifting - 1))**balanceIncentiveCurveExponent[marketIndex];
 
-      uint256 denominator = ((totalLocked >> requiredBitShifting) **
+      uint256 denominator = ((totalLocked >> safeExponentBitShifting) **
         balanceIncentiveCurveExponent[marketIndex]) / 1e18;
 
       uint256 longRewardUnscaled = (numerator / denominator) / 2;
@@ -441,9 +422,9 @@ contract Staker is IStaker, Initializable {
       }
 
       uint256 numerator = (uint256(int256(longValue) + equilibriumOffsetMarketScaled) >>
-        (requiredBitShifting - 1))**balanceIncentiveCurveExponent[marketIndex];
+        (safeExponentBitShifting - 1))**balanceIncentiveCurveExponent[marketIndex];
 
-      uint256 denominator = ((totalLocked >> requiredBitShifting) **
+      uint256 denominator = ((totalLocked >> safeExponentBitShifting) **
         balanceIncentiveCurveExponent[marketIndex]) / 1e18;
 
       uint256 shortRewardUnscaled = (numerator / denominator) / 2;
