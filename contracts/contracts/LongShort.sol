9056e7d9a4b3ead31afbef14408772905e2f69d6
Sean Markham <mrksea002@uct.ac.za>
2021-07-29 08:44:03 +0100

    Add view modifier keyword to assertMArketExists

--- contracts/contracts/LongShort.sol
+++ contracts/contracts/LongShort.sol
@@ -164,7 +164,7 @@ contract LongShort is ILongShort, Initializable {
     _;
   }
 
-  function assertMarketExistsModifierLogic(uint32 marketIndex) internal virtual {
+  function assertMarketExistsModifierLogic(uint32 marketIndex) internal view virtual {
     require(marketExists[marketIndex], "market doesn't exist");
   }
 
