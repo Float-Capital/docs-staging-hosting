352d4f196e46b4f6a1d520866f37cbeef25d4469
denhampreen <denhampreen@gmail.com>
2021-08-04 09:05:13 +0200

    run formatter

--- contracts/contracts/LongShort.sol
+++ contracts/contracts/LongShort.sol
@@ -548,9 +548,9 @@ contract LongShort is ILongShort, Initializable {
         );
       }
 
-      uint256 amountSyntheticTokensToBeShiftedAwayFromOriginSide = userNextPrice_syntheticToken_toShiftAwayFrom_marketSide[
-          marketIndex
-        ][!isLong][user];
+
+        uint256 amountSyntheticTokensToBeShiftedAwayFromOriginSide
+       = userNextPrice_syntheticToken_toShiftAwayFrom_marketSide[marketIndex][!isLong][user];
 
       if (amountSyntheticTokensToBeShiftedAwayFromOriginSide > 0) {
         uint256 syntheticTokenPriceOnOriginSide = syntheticToken_priceSnapshot[marketIndex][
@@ -643,10 +643,10 @@ contract LongShort is ILongShort, Initializable {
     );
 
     uint256 marketAmount = IYieldManager(yieldManagers[marketIndex])
-      .distributeYieldForTreasuryAndReturnMarketAllocation(
-        totalValueLockedInMarket,
-        treasuryYieldPercentE18
-      );
+    .distributeYieldForTreasuryAndReturnMarketAllocation(
+      totalValueLockedInMarket,
+      treasuryYieldPercentE18
+    );
 
     if (marketAmount > 0) {
       if (isLongSideUnderbalanced) {
@@ -769,10 +769,10 @@ contract LongShort is ILongShort, Initializable {
         int256 long_changeInMarketValue_inPaymentToken,
         int256 short_changeInMarketValue_inPaymentToken
       ) = _batchConfirmOutstandingPendingActions(
-          marketIndex,
-          syntheticTokenPrice_inPaymentTokens_long,
-          syntheticTokenPrice_inPaymentTokens_short
-        );
+        marketIndex,
+        syntheticTokenPrice_inPaymentTokens_long,
+        syntheticTokenPrice_inPaymentTokens_short
+      );
 
       newLongPoolValue = uint256(
         int256(newLongPoolValue) + long_changeInMarketValue_inPaymentToken
@@ -1085,9 +1085,9 @@ contract LongShort is ILongShort, Initializable {
     address user,
     bool isShiftFromLong
   ) internal virtual {
-    uint256 syntheticToken_toShiftAwayFrom_marketSide = userNextPrice_syntheticToken_toShiftAwayFrom_marketSide[
-        marketIndex
-      ][isShiftFromLong][user];
+
+      uint256 syntheticToken_toShiftAwayFrom_marketSide
+     = userNextPrice_syntheticToken_toShiftAwayFrom_marketSide[marketIndex][isShiftFromLong][user];
     if (syntheticToken_toShiftAwayFrom_marketSide > 0) {
       uint256 syntheticToken_toShiftTowardsTargetSide = getAmountSyntheticTokenToMintOnTargetSide(
         marketIndex,
