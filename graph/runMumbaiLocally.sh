70b11d8847a2e48e2dca72991ec4e72873f2aa7b
denhampreen <denhampreen@gmail.com>
2021-08-30 19:09:51 +0200

    fix critical log and save synthetic market

--- graph/runMumbaiLocally.sh
+++ graph/runMumbaiLocally.sh
@@ -1,4 +1,5 @@
 docker-compose -f docker-compose.mumbai.yml down -v # make sure docker doesn't have any stale volumes that could cause errors
 docker-compose -f docker-compose.mumbai.yml up -d # start the docker
-sleep 5
+yarn generate-all
+sleep 5 # Pause (sleep) for 5 seconds to allow the docker images/services to finish setting up 
 yarn codegen && yarn create-local && yarn deploy-local-mumbai
