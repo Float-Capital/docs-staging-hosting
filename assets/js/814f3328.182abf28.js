033719de1393aeb0c2700db8bf8748f882928b14
Float-Capital <floatcapital@protonmail.com>
2021-09-15 20:51:21 +0000

    Update from https://github.com/Float-Capital/monorepo/commit/d59a563867bdb34ccc9c55e5165a5c2e5782c216

--- assets/js/814f3328.182abf28.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Introducing Float Capital: The Future of On-Chain Assets Powered by Chainlink","permalink":"/blog/future-onchain-assets"}]}')}}]);
