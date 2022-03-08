49467f72ce1c8adc63ee91db599fac77a0acd67c
Float-Capital <floatcapital@protonmail.com>
2022-03-08 13:19:08 +0000

    Update from https://github.com/Float-Capital/monorepo/commit/143c1ddf7610b7a65717481472852315131d3cb5

--- assets/js/b2b675dd.8ee71980.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[533],{2383:function(e){"use strict";e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":9,"totalPages":2,"totalCount":16,"previousPage":null,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
